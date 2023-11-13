6636087fdcdfd6ff2c53f673efc89f85ca34a169
Jixin Liu <80166502+jxLiu-bio@users.noreply.github.com>
2023-11-13 05:52:26 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     author="Jian Hu, Jixin Liu",
     author_email="jxliu@mail.sdu.edu.cn",
     packages=setuptools.find_packages(),
-    install_requires=["igraph","torch","pandas","numpy","scipy","scanpy","anndata","louvain","scikit-learn", "numba"],
+    install_requires=["igraph","torch","pandas","numpy","scipy","scanpy","anndata","louvain","scikit-learn", "numba", "opencv-python"],
     #install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
